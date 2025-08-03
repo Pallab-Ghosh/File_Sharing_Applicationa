bfc4b586de68f4cc54caf0a5e847dbee9504a86a
PALLAB GHOSH <gpallab405@gmail.com>
2025-08-03 21:33:46 +0530

    Modify CardContent code Add CardDetails component

--- Frontend/my-app/app/components/Content.jsx
+++ Frontend/my-app/app/components/Content.jsx
@@ -1,26 +1,31 @@
+ import Link from "next/link"
+import CardDetails from "./CardDetails"
 
-import React from 'react'
-import Signup from '../(auth)/SignUp/page'
-import { UserRegistration } from './UserRegistration'
 
 const Content = () => {
   return (
-       <div className='flex items-center bg-blue-50 sm:mt-4 flex-col mt-6  w-screen'> 
-          <div className='flex flex-col justify-center gap-4 sm:mt-30 mt-12'>
-                <p className='text-6xl text-center font-bold'>Share Documents</p>
-                <p className='text-6xl text-center text-blue-700  font-bold'>Collaborate Seamlessly</p>
-                <div className='mt-8 flex flex-col text-center'>
-                   <p className='sm:text-2xl text-lg  text-gray-600'> 
-                     Upload, share, and collaborate on documents with your team. Comment,
-                     </p>
-                   <p className='sm:text-2xl text-lg  text-gray-600'>
-                     download, and manage your files all in one secure platform.
-                     </p>
+       <div className='flex justify-center items-center bg-sky-50 mt-5 flex-col'> 
+          <div className='flex flex-col justify-center gap-4 mt-20'>
+                <p className='text-5xl text-center font-bold'>Share Documents</p>
+                <p className='text-5xl text-center text-blue-700  font-bold'>Collaborate Seamlessly</p>
+                <div className='mt-8 flex flex-col'>
+                   <p className='text-2xl text-center text-gray-600'> Upload, share, and collaborate on documents with your team. Comment,</p>
+                   <p className='text-2xl text-center  text-gray-600'>download, and manage your files all in one secure platform.</p>
                 </div>
           </div>
-            <UserRegistration/>
+            <div className='mt-5 flex justify-between gap-5'>
+               <Link href={'/Signup'}>
+                  <div className='bg-blue-700 flex text-amber-50 w-50 justify-center h-10 items-center text-xl rounded-md hover:bg-blue-500'> Get Started Free</div>
+               </Link>
+               <Link href={'/Signin'}>
+                   <div className='bg-white hover:bg-blue-100 flex text-shadow-black font-se w-32 justify-center h-10 items-center text-xl rounded-md '> Sign In</div>
+               </Link>
+            </div>
+            <div>
+              <CardDetails/>
+            </div>
         </div>
   )
 }
 
-export default Content
+export default Content
