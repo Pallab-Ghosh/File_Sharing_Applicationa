0067b9fed7b043b91d0f968d74a5e9e19709e69c
pallabquickelf <pallab.ghosh@quickelf.com>
2025-08-05 13:09:32 +0530

    Update Structure

--- Frontend/my-app/app/(Landing)/page.jsx
+++ Frontend/my-app/app/(Landing)/page.jsx
@@ -6,14 +6,14 @@ import Content from '../components/Content'
 
 const FrontPage = () => {
   return (
-       <>
+       <div>
         <div className='flex gap-2 content-center ml-5 mt-5'>
          <div> <FileText color='blue' size={40}/></div>
           <p className='text-2xl font-bold items-center flex'>ShareDoc</p>
         </div>
          <Content/>
-        <Content/>
-       </>
+        
+       </div>
   )
 }
 
