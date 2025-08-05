452ced52c1eb01de8a7804f874f7b207ac61a0e6
pallabquickelf <pallab.ghosh@quickelf.com>
2025-08-05 13:10:09 +0530

    Create MoreContent.jsx

---
+++ Frontend/my-app/app/components/MoreContent.jsx
@@ -0,0 +1,13 @@
+import HomeBtn from "./HomeBtn"
+
+const MoreContent = () => {
+  return (
+    <div className="w-screen h-80 text-center">
+       <p className="font-bold text-3xl sm:text-4xl mt-16">Ready to start sharing?</p>
+       <p className="text-lg sm:text-xl mt-6">Join thousands of teams who trust ShareDoc for their document collaboration needs.</p>
+        <HomeBtn/>
+    </div>
+  )
+}
+
+export default MoreContent
