e7e93eabeff0168c634946907adfd112ad6b30f8
pallabquickelf <pallab.ghosh@quickelf.com>
2025-08-05 17:29:42 +0530

    Update Styling of this component

--- Frontend/my-app/app/components/HomeBtn.jsx
+++ Frontend/my-app/app/components/HomeBtn.jsx
@@ -1,11 +1,19 @@
 'use client'
 
+import { useRouter } from "next/navigation"
+
 
 const HomeBtn = () => {
+  const router =useRouter()
   return (
-    <div className=" bg-blue-700">
-        <button>Start Free Today</button>
-    </div>
+    <div className="flex justify-center mt-10">
+        <button 
+        className="h-12 bg-blue-700 w-52 rounded-lg text-white sm:text-2xl sm:w-60 font-bold"
+        onClick={()=>router.push('/')}//as of now home page later go on dashboard page with ID
+        >
+          Start Free Today
+          </button>
+    </div> 
   )
 }
 
