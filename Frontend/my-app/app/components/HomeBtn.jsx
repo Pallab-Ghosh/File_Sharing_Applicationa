8575dd52ab8e5feb3a57b8b844c4dae7fd3ac674
pallabquickelf <pallab.ghosh@quickelf.com>
2025-08-06 22:49:15 +0530

    Change the Stylings

--- Frontend/my-app/app/components/HomeBtn.jsx
+++ Frontend/my-app/app/components/HomeBtn.jsx
@@ -4,16 +4,16 @@ import { useRouter } from "next/navigation"
 
 
 const HomeBtn = () => {
-  const router =useRouter()
+  const router = useRouter()
   return (
     <div className="flex justify-center mt-10">
-        <button 
+      <button
         className="h-12 bg-blue-700 w-52 rounded-lg text-white sm:text-2xl sm:w-60 font-bold"
-        onClick={()=>router.push('/')}//as of now home page later go on dashboard page with ID
-        >
-          Start Free Today
-          </button>
-    </div> 
+        onClick={() => router.push('/')}//TODO[:as of now go to home page later go on dashboard page]
+      >
+        Start Free Today
+      </button>
+    </div>
   )
 }
 
