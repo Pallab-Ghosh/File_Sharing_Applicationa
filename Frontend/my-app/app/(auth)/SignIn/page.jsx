4495f7452ead0aa082b66053a47d861416e605df
PALLAB GHOSH <gpallab405@gmail.com>
2025-08-09 20:59:33 +0530

    Update page of SignIn and Integrate <SignInForm/> component

--- Frontend/my-app/app/(auth)/SignIn/page.jsx
+++ Frontend/my-app/app/(auth)/SignIn/page.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import SignUpForm from '../../components/SignInForm';
+import SignInForm from '../../components/SignInForm';
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+       <div>
+           <p className='font-bold text-xl'>Welcome Back</p>
+           <p className='font-semibold text-lg'>Sign in to access your documents</p>
+       </div>
+        <div>
+           <SignInForm/>
+        </div>
+    </div>
   )
 }
 
