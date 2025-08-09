ef9864720913699f2d77eac026c0c2d8aac0834a
PALLAB GHOSH <gpallab405@gmail.com>
2025-08-09 21:00:36 +0530

    Create SignInForm.jsx

---
+++ Frontend/my-app/app/components/SignInForm.jsx
@@ -0,0 +1,70 @@
+'use client'
+import React from 'react'
+import * as Yup from 'yup';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import Link from 'next/link';
+
+//Define the Schema for Form
+const validation_Schema = Yup.object({
+  email: Yup.string().email('Invalid Email').required('Email is required'),
+  password: Yup.string().min(6, "Password must be atleast 6 characters").required("Please Provide Password"),
+})
+
+const SignInForm = () => {
+    
+    //Crete a Default value 
+const initial_Values = {email: '',password: "" };
+
+    //After Form Submission handleSubmit func will run
+    const handleSubmit = (values, { setSubmitting, resetForm }) => {
+      alert(JSON.stringify(values));
+      resetForm();
+  };
+
+
+return (
+    <div> 
+        <Formik initialValues={initial_Values} validationSchema={validation_Schema} onSubmit={handleSubmit}>
+        <Form className="flex flex-col gap-4">
+            <div>
+            <label className="block text-gray-700">Email</label>
+            <Field
+                name="email"
+                type="email"
+                className="w-full border p-2 rounded"
+                placeholder="Enter your email"
+            />
+            <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
+            </div>
+
+        
+            <div>
+            <label className="block text-gray-700">Password</label>
+            <Field
+                name="password"
+                className="w-full border p-2 rounded"
+                placeholder="Enter your password"
+            />
+            <ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
+            </div>
+
+
+            <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+            >
+            Sign-ins
+            </button>
+            <div className='text-center'>
+            <p className='text-xl'>Dont have an account?</p>
+            <Link href='/SignUp'>
+                <p className=' text-blue-800 font-bold textlg'>Sign Up</p>
+            </Link>
+            </div>
+        </Form>
+        </Formik>
+    </div>
+  )
+}
+
+export default SignInForm
