ab02c7f743c3960984eb7800e005713fba21db31
PALLAB GHOSH <gpallab405@gmail.com>
2025-08-09 21:41:03 +0530

    Update more stylings

--- Frontend/my-app/app/(auth)/SignIn/page.jsx
+++ Frontend/my-app/app/(auth)/SignIn/page.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import SignUpForm from '../../components/SignInForm';
 import SignInForm from '../../components/SignInForm';
+import { FileText } from 'lucide-react';
 const page = () => {
   return (
-    <div>
-       <div>
-           <p className='font-bold text-xl'>Welcome Back</p>
-           <p className='font-semibold text-lg'>Sign in to access your documents</p>
-       </div>
-        <div>
-           <SignInForm/>
+    <div className='h-screen flex justify-center items-center'>
+        <div className='shadow-2xl w-96 sm:w-lg'>
+            <div className='mb-20 text-center flex flex-col items-center'>
+               <FileText size={40} color='blue' className='mb-7' />
+              <p className='font-bold text-4xl mb-3'>Welcome Back</p>
+              <p className=' text-lg'>Sign in to access your documents</p>
+          </div>
+            <div>
+              <SignInForm/>
+            </div>
         </div>
     </div>
   )
