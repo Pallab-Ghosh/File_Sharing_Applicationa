73ab4715110819e8a4fcc1716979ae5449fa1d08
PALLAB GHOSH <gpallab405@gmail.com>
2025-08-23 19:11:19 +0530

    Replace the Link of  Feedback and Contact section by Dialog component

--- Frontend/my-app/app/components/FeedbackandContact.jsx
+++ Frontend/my-app/app/components/FeedbackandContact.jsx
@@ -1,15 +1,17 @@
-import Link from "next/link"
+
+import  DialogContents from './DialogContents'
 
 const FeedbackandContact = () => {
    return (
 
-      <div className="flex mt-5 sm:gap-3 p-1 justify-between sm:mr-5 ml-2">
-         <Link href={'/SignUp'}>
-            <div className='bg-white flex text-shadow-black font-semibold w-28 sm:w-32 sm:text-xl sm:h-14 justify-center h-8 items-center text-xl rounded-md hover:bg-blue-100 '> Feedback</div>
-         </Link>
-         <Link href={'/SignIn'}>
-            <div className='bg-blue-600  hover:bg-blue-800 text-white  sm:w-32 sm:h-14 sm:text-xl  flex text-shadow-black  w-28 justify-center h-8 items-center text-xl rounded-sm '> Contact us</div>
-         </Link>
+      <div className="flex mt-5 sm:gap-3 p-1 justify-between sm:mr-5 ml-3 gap-1">
+            <div className='bg-white flex text-shadow-black font-semibold w-28 sm:w-32 sm:text-xl sm:h-14 justify-center h-8 items-center text-xl rounded-md hover:bg-blue-100 '>
+               <DialogContents type="feedback"/>
+                </div>  
+            <div className='bg-blue-600  hover:bg-blue-800 text-white  sm:w-32 sm:h-14 sm:text-xl  flex text-shadow-black  w-28 justify-center h-8 items-center text-xl rounded-sm '> 
+                <DialogContents type="Contact us"/>
+               </div>
+
       </div>
 
    )
