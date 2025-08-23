bdd81845bd5e75bb442c0f36d2ca1ee4510d094c
PALLAB GHOSH <gpallab405@gmail.com>
2025-08-23 19:07:16 +0530

    Fixed the Landing Page Styling issue

--- Frontend/my-app/app/(Landing)/page.jsx
+++ Frontend/my-app/app/(Landing)/page.jsx
@@ -6,7 +6,7 @@ const FrontPage = () => {
   return (
     <div>
       <div className='flex sm:justify-between sm:items-center'>
-        <div className='flex gap-1 item-center ml-mr-2 sm:mt-5 sm:mr-5 mt-6'>
+        <div className='flex gap-1 item-center ml-2 mr-2  sm:mr-5 mt-5 sm:gap-2'>
           <div>
             <FileText color='blue' size={32} className='sm:size-10 sm:ml-3' />
           </div>
